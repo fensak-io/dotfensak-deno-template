24bc901df09857f7a07371e5a999443d7d4f8d06
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-10-19 13:26:38 -0500

    Bump reng to latest version
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- rules/sample_test.ts
+++ rules/sample_test.ts
@@ -1,12 +1,13 @@
 import { assert } from "https://deno.land/std@0.202.0/testing/asserts.ts";
 import {
   compileRuleFn,
+  emptyChangeSetMetadata,
   IGitHubRepository,
   patchFromGitHubPullRequest,
   RuleFnSourceLang,
   RuleLogMode,
   runRule,
-} from "npm:@fensak-io/reng@^1.1.3";
+} from "npm:@fensak-io/reng@^1.2.1";
 import { Octokit } from "npm:@octokit/rest@^20.0.0";
 
 const __dirname = new URL(".", import.meta.url).pathname;
@@ -24,10 +25,7 @@ const testRepo: IGitHubRepository = {
 const opts = { logMode: RuleLogMode.Console };
 
 Deno.test("No changes should be approved", async () => {
-  const result = await runRule(ruleFn, [], {
-    sourceBranch: "foo",
-    targetBranch: "bar",
-  }, opts);
+  const result = await runRule(ruleFn, [], emptyChangeSetMetadata, opts);
   assert(result.approve);
 });
 
